9ef18f57d3c0cf1d1d8d5a9a88a322c9cf93b87e
wangjiapu <857484057@qq.com>
2017-11-14 15:35:18 +0800

    Initial commit

---
+++ src/com/pjw/servlet/Login.kt
@@ -0,0 +1,18 @@
+package com.pjw.servlet
+
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+@WebServlet("/loginservlet")
+class Login:HttpServlet() {
+    override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
+        super.doGet(req, resp)
+    }
+
+    override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
+        val name=req.getParameter("name")
+        val pwd=req.getParameter("pwd")
+        resp.writer.print("${name}----${pwd}")
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: PUJW
+  Date: 2017/11/14
+  Time: 15:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+ <form action="/loginservlet" method="post">
+
+   name:<input type="text" name="name" value=""><br/>
+   pwd:<input type="text" name="pwd" value=""><br/>
+   <input type="submit" value="提交">
+ </form>
+  </body>
+</html>
